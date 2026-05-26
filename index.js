a3d9ad9e082e985ce20e40930c0c3b2b4fc882a3
mdsajuislam764263-max <mdsajuislam764263@gmail.com>
2026-05-27 03:42:39 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,165 @@
-/**
- * @author NTKhang & Modded by NeoKEX
- * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
- * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
- * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
- *
- * English:
- * ! Please do not change the below code, it is very important for the project.
- * It is my motivation to maintain and develop the project for free.
- * ! If you change it, you will be banned forever
- * Thank you for using
- *
- * Vietnamese:
- * ! Vui lòng không thay đổi mã bên dưới, nó rất quan trọng đối với dự án.
- * Nó là động lực để tôi duy trì và phát triển dự án miễn phí.
- * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn
- * Cảm ơn bạn đã sử dụng
- */
+module.exports = async ({ api, event }) => {
+  const logger = require('./Nayan/catalogs/Nayanc.js')
+  
+  const configCustom = {
+    autosetbio: {
+      status: false,
+      bio: `prefix : ${global.config.PREFIX}`,
+      note: 'automatically change the bot bio.'
+    },
+    notification: {
+      status: false,
+      time: 39, // 39 minutes
+      note: 'bot will update you on his informations like all users, all groups, all operators, all admins every 30 minutes'
+    },
+    greetings: {
+      status: false,
+      morning: `goodmorning everyone, have a nice day.`,
+      afternoon: `goodafternoon everyone, don't forget to eat your lunch.`,
+      evening: `goodevening everyone, don't forget to eat.`,
+      sleep: `goodnight everyone, time to sleep.`,
+      note: 'greetings every morning, afternoon and evening. the timezone is located in Asia/Dhaka'
+    },
+    reminder: {
+      status: false,
+      time: 40, // 40 minutes
+      msg: 'reminder test',
+      note: 'this is a reminder for 40 minutes, you can disabled it by setting the status to false'
+    },
+    autoDeleteCache: {
+      status: true,
+      time: 10, // 10 minutes
+      note: 'auto delete caches, kindly set the status to true, if you dont want to delete caches, set the status to false.'
+    },
+    autoRestart: {
+      status: true,
+      time: 40, // 40 minutes
+      note: 'to avoid problems, enable periodic bot restarts, set the status to false if you want to disable auto restart function.'
+    },
+    accpetPending: {
+      status: false,
+      time: 10, // 10 minutes
+      note: 'approve waiting messages after a certain time, set the status to false if you want to disable auto accept message request.'
+    }
+  }
 
-const { spawn } = require("child_process");
-const log = require("./logger/log.js");
-
-function startProject() {
-        const child = spawn("node", ["Goat.js"], {
-                cwd: __dirname,
-                stdio: "inherit",
-                shell: true
-        });
-
-        child.on("close", (code) => {
-                log.info("Project stopped with code:", code);
-                if (code === 2) {
-                        log.info("Project", "Restarting...");
-                        startProject();
+  function autosetbio(config) {
+    if (config.status) {
+      try {
+        api.changeBio(config.bio, (err) => {
+          if (err) {
+            logger(`having some unexpected error : ${err}`, 'setbio')
+          }; return logger(`changed the bot bio into : ${config.bio}`, 'setbio')
+        })
+      } catch (error) {
+        logger(`having some unexpected error : ${error}`, 'setbio')
+      }
+    }
+  }
+  function notification(config) {
+    if (config.status) {
+      setInterval(async () => {
+        const operator = global.config.OPERATOR[0];
+        api.sendMessage(`bot information\n\nusers : ${global.data.allUserID.length}\ngroups : ${global.data.allThreadID.length}\noperators : ${global.config.OPERATOR.length}\nadmins : ${global.config.ADMINBOT.length}`, operator)
+      }, config.time * 60 * 1000)
+    }
+  }
+  function greetings(config) {
+    if (config.status) {
+      try {
+      const nam = [
+        {
+          timer: '6:00:00 AM',
+          message: [`${config.morning}`]
+        },
+        {
+          timer: '12:00:00 AM',
+          message: [`${config.afternoon}`]
+        },
+        {
+          timer: '6:00:00 PM',
+          message: [`${config.evening}`]
+        },
+        {
+          timer: '10:00:00 PM',
+          message: [`${config.sleep}`]
+        }
+      ];
+        setInterval(() => {
+const r = a => a[Math.floor(Math.random()*a.length)];
+if (á = nam.find(i => i.timer == new Date(Date.now()+25200000).toLocaleString().split(/,/).pop().trim())) global.data.allThreadID.forEach(i => api.sendMessage(r(á.message), i));
+}, 1000);
+      } catch (error) {
+        logger(`having some unexpected error : ${error}`, 'greetings')
+      }
+    }
+  }
+  function reminder(config) {
+    if (config.status) {
+      setInterval(async () => {
+        let allThread = global.data.allThreadID || [];
+        await new Promise(resolve => {
+          allThread.forEach((each) => {
+            try {
+              api.sendMessage(config.msg, each, (err, info) => {
+                if (err) {
+                  logger(`having some unexpected error : ${err}`, 'reminder')
                 }
-        });
-}
+              })
+            } catch (error) {
+              logger(`having some unexpected error : ${error}`, 'reminder')
+            }
+          })
+        })
+      }, config.time * 60 * 1000)
+    }
+  }
+  function autoDeleteCache(config) {
+    if(config.status) {
+      setInterval(async () => {
+        const { exec } = require('child_process');
+        exec('rm -rf ../../scripts/commands/cache && mkdir -p ../../scripts/commands/cache && rm -rf ../../scripts/events/cache && mkdir -p ../../scripts/events/cache', (error, stdout, stderr) => {
+        if (error) {
+          logger(`error : ${error}`, "cache")
+          return;
+        }
+        if (stderr) {
+          logger(`stderr : ${stderr}`, "cache")
+          return;
+        }
+        return logger(`successfully deleted caches`, "cache")
+        })
+      }, config.time * 60 * 1000)
+    }
+  }
+  function autoRestart(config) {
+    if(config.status) {
+      setInterval(async () => {
+        logger(`auto restart is processing, please wait.`, "Nayan")
+        process.exit(1)
+      }, config.time * 60 * 1000)
+    }
+  }
+  function accpetPending(config) {
+    if(config.status) {
+      setInterval(async () => {
+          const list = [
+              ...(await api.getThreadList(1, null, ['PENDING'])),
+              ...(await api.getThreadList(1, null, ['OTHER']))
+          ];
+          if (list[0]) {
+              api.sendMessage('this thread is automatically approved by our system.', list[0].threadID);
+          }
+      }, config.time * 60 * 1000)
+    }
+  }
 
-startProject();
+autosetbio(configCustom.autosetbio)
+notification(configCustom.notification)
+greetings(configCustom.greetings)
+reminder(configCustom.reminder)
+autoDeleteCache(configCustom.autoDeleteCache)
+autoRestart(configCustom.autoRestart)
+accpetPending(configCustom.accpetPending)
+	
+};
